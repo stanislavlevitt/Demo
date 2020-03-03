e8ec10e8a4e70c91ed9e335a69ddc01ac88c5ce6
Ya-sin <ya-sinzenk@hotmail.fr>
2020-03-03 15:13:39 -0500

    components

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -21,6 +21,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <Link to="/products">Products</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="/cart">Cart</Link>
         </div>
       )}
     </nav>
