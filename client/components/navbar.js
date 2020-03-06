a09fb678de158ac598b50cf512f4787e2e4f309a
Trevor <take1seet@gmail.com>
2020-03-05 19:15:28 -0500

    css navigation component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,25 +5,42 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
+  <div id="navLinks">
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          {/* <Link to="/home">Home</Link>
           <Link to="/products">Products</Link>
           <Link to="/cart">Cart</Link>
           <a href="#" onClick={handleClick}>
             Logout
-          </a>
+          </a> */}
+
+          <h4>
+            <a href="/home">Home</a>
+            <a href="/products">Products</a>
+            <a href="/cart">Cart</a>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </h4>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/products">Products</Link>
+          {/* <Link to="/products">Products</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">Cart</Link>
+          <Link to="/cart">Cart</Link> */}
+          <h4>
+            <a href="/home">Home</a>
+            <a href="/products">Products</a>
+            <a href="/cart">Cart</a>
+            <a href="/login">Login</a>
+            <a href="/signup">Sign Up</a>
+            <a href="/cart">Cart</a>
+          </h4>
         </div>
       )}
     </nav>
